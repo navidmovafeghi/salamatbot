73b91abdec782895b38e64cec441e44d62be2416
navidmovafeghi <movafeginavid90@gmail.com>
2025-06-08 09:59:35 +0330

    feat(dev-env): configure prettier and eslint for workspace

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,15 +1,14 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-      'plugin:@typescript-eslint/recommended',
-      'plugin:prettier/recommended',
-    ],
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-    },
-    rules: {
-      // You can add custom rules here
-    },
-  
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    // You can add custom rules here
+  },
 };
