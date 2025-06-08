b44459cb062cc4332ce336ae6fe00e0e81014650
navidmovafeghi <movafeginavid90@gmail.com>
2025-06-08 13:28:32 +0330

    switch to ESLint flat config

---
+++ backend/eslint.config.js
@@ -0,0 +1,29 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...prettierPlugin.configs.recommended.rules,
+      // Add your custom rules here
+    },
+  },
+  {
+    rules: prettierConfig.rules,
+  },
+]; 
