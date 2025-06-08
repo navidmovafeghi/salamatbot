9a410066a7755e61c6bf956b96457ab7e92e7d8a
navidmovafeghi <movafeginavid90@gmail.com>
2025-06-08 13:04:00 +0330

    feat(ci): add smoke test to verify pipeline

---
+++ backend/jest.config.js
@@ -0,0 +1,6 @@
+// backend/jest.config.js
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+  };
---
+++ backend/src/index.test.js
@@ -0,0 +1,7 @@
+// backend/src/index.test.ts
+
+describe('Initial Test', () => {
+    it('should always be true to pass the smoke test', () => {
+      expect(true).toBe(true);
+    });
+  });
